854dd23cafb31a848d5e85a694dc864fe286c883
bef <bef@pentaphase.de>
2011-02-15 18:16:54 +0000

    .

---
+++ test/run.tcl
@@ -0,0 +1,11 @@
+#!/usr/bin/tclsh8.5
+if {[lsearch -exact $auto_path {..}] < 0} {
+	set auto_path [linsert $auto_path 0 [file join [file dirname [info script]] ..]]
+}
+package require Tcl 8.5
+package require tcltest 2.2
+
+::tcltest::configure -testdir \
+	[file dirname [file normalize [info script]]]
+eval ::tcltest::configure $argv
+::tcltest::runAllTests
