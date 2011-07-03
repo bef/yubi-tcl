65bf782b9b6d67f145fd70564939375822cfefc4
bef <bef@pentaphase.de>
2011-07-03 15:08:18 +0000

    support for libpam-script

--- examples/ssh_force_cmd.tcl
+++ examples/ssh_force_cmd.tcl
@@ -1,6 +1,7 @@
 #!/usr/bin/tclsh8.5
 #
 # check OTP with OpenSSH ForceCommand for two-factor authentication
+# or check OTP with libpam-script / pam_script_auth
 #     Copyright (C) 2011 - Ben Fuhrmannek <bef@pentaphase.de>
 # 
 #     This program is free software: you can redistribute it and/or modify
@@ -24,34 +25,42 @@ package require yubi::wsapi::client
 package require inifile
 
 proc log {msg {level auth.notice}} {
-	# puts stderr $msg
-	exec logger -p $level -i -t OTP "$::env(USER): $msg"
+	if {[info exists ::auth_user]} {set msg "$::auth_user: $msg"}
+	exec logger -p $level -i -t OTP $msg
 }
 
 proc permission_denied {} {
 	puts stderr "permission denied."
-	exit
+	exit 1
 }
 
 ## args
-set inifile [lindex $::argv 0]
-if {$inifile == ""} {set inifile "/opt/yubi/etc/authorized_yubi.ini"}
-
-## debugging
-# foreach {k v} [array get ::env] {
-# 	puts stderr "$k = $v"
-# }
-# puts stderr "---"
+proc find_inifile {} {
+	foreach arg $::argv {
+		set kv [split $arg "="]
+		if {[lindex $kv 0] eq "ini"} {return $v}
+	}
+}
+set inifile [find_inifile]
+if {$inifile eq ""} {set inifile "/opt/yubi/etc/authorized_yubi.ini"}
+if {[info exists env(AUTHORIZED_YUBI)]} {set inifile $env(AUTHORIZED_YUBI)}
 
 ## get OTP string
-if {[info exists env(SSH_AUTH_OTP)]} {
-	## get OTP from env(SSH_AUTH_OTP)
+if {[info exists env(PAM_AUTHTOK)]} {
+	set auth_mode pam
+	set input $env(PAM_AUTHTOK)
+	set auth_user $env(PAM_USER)
+} elseif {[info exists env(SSH_AUTH_OTP)]} {
+	set auth_mode ssh
 	set input $env(SSH_AUTH_OTP)
+	set auth_user $env(USER)
 } else {
+	set auth_mode ssh
 	## get OTP from stdin
 	puts -nonewline stdout "One-Time-Password: "
 	flush stdout
 	gets stdin input
+	set auth_user $env(USER)
 }
 
 ## normalize/un-dvorak input
@@ -62,23 +71,25 @@ if {[catch {
 	permission_denied
 }
 
-## check env
-foreach v {SHELL} {
-	if {![info exists env($v)]} {
-		log "missing env($v)"
-		permission_denied
+## check env (ssh mode)
+if {$auth_mode eq "ssh"} {
+	foreach v {SHELL} {
+		if {![info exists env($v)]} {
+			log "missing env($v)"
+			permission_denied
+		}
 	}
 }
 
 ## get config
 set ini [::ini::open $inifile r]
-if {![::ini::exists $ini "user:$::env(USER)"]} {
+if {![::ini::exists $ini "user:$auth_user"]} {
 	log "missing user in $inifile"
 	::ini::close $ini
 	permission_denied
 }
 set global_config [::ini::get $ini global]
-set user_config [::ini::get $ini "user:$::env(USER)"]
+set user_config [::ini::get $ini "user:$auth_user"]
 ::ini::close $ini
 set config [dict merge $global_config $user_config]
 
@@ -117,17 +128,21 @@ if {[catch {
 
 log "success"
 
-## success -> check for SSH_ORIGINAL_COMMAND
-if {[catch {package require Tclx}]} {
-	puts stderr "sorry. contact your system administrator."
-	log "Tclx not installed :("
-	exit
-}
-proc special_exec {cmd args} {execl $cmd $args}
+if {$auth_mode eq "ssh"} {
+	## success -> check for SSH_ORIGINAL_COMMAND
+	if {[catch {package require Tclx}]} {
+		puts stderr "sorry. contact your system administrator."
+		log "Tclx not installed :("
+		exit
+	}
+	proc special_exec {cmd args} {execl $cmd $args}
 
-if {[info exists env(SSH_ORIGINAL_COMMAND)]} {
-	special_exec {*}$env(SSH_ORIGINAL_COMMAND)
-} else {
-	special_exec $env(SHELL) -l
+	if {[info exists env(SSH_ORIGINAL_COMMAND)]} {
+		special_exec {*}$env(SSH_ORIGINAL_COMMAND)
+	} else {
+		special_exec $env(SHELL) -l
+	}
 }
+
+exit 0
 
